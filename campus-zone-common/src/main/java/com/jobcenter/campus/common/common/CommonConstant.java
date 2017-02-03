8087c9b51ebbeb6ba4f07d5d11a7e2db81bfa933
clement <lzy.clement@gmail.com>
2017-02-03 15:23:54 +0800

    init(project) : init perf4j jar

---
+++ campus-zone-common/src/main/java/com/jobcenter/campus/common/common/CommonConstant.java
@@ -0,0 +1,16 @@
+package com.jobcenter.campus.common.common;
+
+/**
+ * <p>
+ * <br>==========================
+ * <br> 公司：job-center
+ * <br> 系统：campus-zone
+ * <br> 开发：lzy.clement
+ * <br> 创建时间：2017/2/3
+ * <br>==========================
+ */
+public class CommonConstant {
+
+    //在request attribut中存在的用于计时的StopWatch的name
+    public static final String STOPWATCH = "stopWatch";
+}
